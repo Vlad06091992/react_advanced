d0758523e8d2a52d1565961ac5f28f8fdf86519e
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-04-14 12:29:38 +0500

    refactor

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,17 +1,41 @@
 import path from "path";
 import webpack from "webpack";
-import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 import {BuildOptions, ENV} from "./config/build/types/BuildOptions";
+import {buildLoaders} from "./config/build/buildLoaders";
+import {buildResolvers} from "./config/build/buildResolvers";
+import {buildPlugins} from "./config/build/buildPlugins";
+import {buildDevServer} from "./config/build/buildDevServer";
 
-const options:BuildOptions = {
-    paths:{
-        build: path.resolve(__dirname, 'build'),
-        entry: path.resolve(__dirname, 'src', 'index.ts'),
-        html:path.resolve(__dirname, 'public', 'index.html')
-    },
-    isDev:true,
+export default (env:ENV): webpack.Configuration => {
+
+    const options:BuildOptions = {
+        paths:{
+            build: path.resolve(__dirname, 'build'),
+            entry: path.resolve(__dirname, 'src', 'index.ts'),
+            html:path.resolve(__dirname, 'public', 'index.html')
+        },
+        isDev:env.mode === 'development',
+    }
+
+    const {paths: {build, entry, html}, isDev = true} = options
+
+    return {
+        devtool: isDev ? 'inline-source-map': null,
+        mode: env.mode,
+        entry: entry,
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+        output: {
+            filename: "[name].[contenthash].js",
+            path: build,
+            clean: true
+        },
+        plugins: buildPlugins(html),
+        stats: {
+            children: true,
+        },
+        devServer:isDev ? buildDevServer(options,env) : null
+    }
 }
-
-
-
-export default  (env:ENV) => buildWebpackConfig(options,env)
