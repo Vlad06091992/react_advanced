4a7a2d55bd5cf79169beab94df5d93709b7a2028
vlad <smirnov06091992@yandex.ru>
2025-04-17 20:42:31 +0500

    create btn component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React, {ButtonHTMLAttributes, FC} from "react";
+import {classnames} from "shared/lib/classnames";
+import styles from './Button.module.scss'
+
+export enum ThemeButton {
+    CLEAR = 'clear',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    theme?: ThemeButton
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+    const {className, children, theme = ThemeButton.CLEAR, ...restProps} = props
+    return <button className={classnames(className,[styles.clear],{}, true)} {...restProps}>{children}</button>
+}
