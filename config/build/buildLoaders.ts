6525f2b2e1fd85464d03d0baffc9aae94a2437ec
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-04-19 10:52:41 +0500

    add babel loader and i18n extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,6 +5,27 @@ import {BuildOptions} from "../build/types/BuildOptions";
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     const {isDev} = options
 
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
+    }
+
     const fileLoader = {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
@@ -41,8 +62,12 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
             "sass-loader",
         ],
     };
+
+    //порядок лоадеров важен!
+
     return [
         fileLoader, svgLoader,
-        typescriptLoader, scssLoader
+        // babelLoader,
+        typescriptLoader,  scssLoader
     ]
 }
