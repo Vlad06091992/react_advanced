c1fa42bb725ea3898e44563a45942d063afd0ffa
vlad <smirnov06091992@yandex.ru>
2025-04-20 18:58:30 +0500

    added tests for classnames

--- src/shared/lib/classnames.test.ts
+++ src/shared/lib/classnames.test.ts
@@ -1,8 +1,41 @@
-// import { classnames } from 'shared/lib/classnames';
 import { classnames } from './classnames';
 
-describe('init', () => {
-    it('should ', () => {
-        expect(classnames('hz')).toBe('hz');
+describe('classnames', () => {
+    test('with only first param', () => {
+        expect(classnames('someClass')).toBe('someClass');
+    });
+
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2';
+        expect(classnames('someClass', ['class1', 'class2'], {}))
+            .toBe(expected);
+    });
+
+    test('with mods', () => {
+        const expected = 'someClass hovered scrollable class1 class2';
+        expect(classnames(
+            'someClass',
+            ['class1', 'class2'],
+            { hovered: true, scrollable: true },
+        )).toBe(expected);
+    });
+
+    test('with mods false', () => {
+        const expected = 'someClass hovered class1 class2';
+        expect(classnames(
+            'someClass',
+            ['class1', 'class2'],
+            { hovered: true, scrollable: false },
+        )).toBe(expected);
+    });
+
+    test('with mods undefined', () => {
+        const expected = 'someClass hovered class1 class2';
+        expect(classnames(
+            'someClass',
+
+            ['class1', 'class2'],
+            { hovered: true, scrollable: undefined },
+        )).toBe(expected);
     });
 });
