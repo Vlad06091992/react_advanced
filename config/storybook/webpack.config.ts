0aea50e659c5fe93136bfa6d0e7a134281a89a58
vlad <smirnov06091992@yandex.ru>
2025-04-26 08:52:31 +0500

    upd

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -2,7 +2,6 @@ import webpack, { RuleSetRule } from 'webpack';
 import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/BuildOptions';
-// import { BuildPaths } from '../build/types/config';
 
 export default ({ config }: {config: webpack.Configuration}) => {
     const paths: BuildPaths = {
