48904748deb12d2de07926bbf36c5d67bd924dd4
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-05-04 09:17:42 +0500

    set up react hooks linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
         'react',
         '@typescript-eslint',
         'i18next',
+        'react-hooks',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -45,6 +46,8 @@ module.exports = {
             'warn',
             { ignoreComments: true, code: 120 },
         ],
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
     },
     globals: {
         __IS_DEV__: true,
