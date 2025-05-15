e3db4f880e5bfbfb811ebbdffff4fb0921cbf8e1
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-05-15 11:16:17 +0500

    37 - upd storybook

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import ProfilePage from './ProfilePage';
+import ProfilePage from 'pages/ProfilePage/ui/ProfilePage';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 
 export default {
-    title: 'pages/AboutPage',
+    title: 'pages/ProfilePage',
     component: ProfilePage,
     argTypes: {
         backgroundColor: { control: 'color' },
@@ -16,7 +17,8 @@ const Template: ComponentStory<typeof ProfilePage> = (args) => <ProfilePage {...
 
 export const Normal = Template.bind({});
 Normal.args = {};
+Normal.decorators = [StoreDecorator({})];
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
