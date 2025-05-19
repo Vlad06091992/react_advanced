d3610d8be60cee731330498be07710ec87b015eb
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-05-19 10:58:39 +0500

    comment temp unused

--- src/entities/Profile/ui/Counter.tsx
+++ src/entities/Profile/ui/Counter.tsx
@@ -1,30 +1,33 @@
-import { classnames } from 'shared/lib/classnames';
-import { FC } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
-import { getCounterValue } from '../model/selectors/getCounterValue';
-import { counterActions } from '../model/slice/CounterSlice';
+// import { classnames } from 'shared/lib/classnames';
+// import { FC } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+// // import { counterActions } from 'entities/Counter/model/slice/CounterSlice';
+// import { getCounterValue } from '../model/selectors/getCounterValue';
+// import { counterActions } from '../model/slice/CounterSlice';
+//
+// export interface CounterProps {
+//     className?:string
+// }
+//
+// export const Counter:FC<CounterProps> = ({ className }) => {
+//     const dispatch = useDispatch();
+//     const value = useSelector<StateSchema>(getCounterValue);
+//
+//     const inc = () => {
+//         dispatch(counterActions.increment());
+//     };
+//     const dec = () => {
+//         dispatch(counterActions.decrement());
+//     };
+//
+//     return (
+//         <div className={classnames(classnames(className))}>
+//             <h1 data-testid="value-title">{value}</h1>
+//             <button data-testid="increment-btn" onClick={inc}>inc</button>
+//             <button data-testid="decrement-btn" onClick={dec}>dec</button>
+//         </div>
+//     );
+// };
 
-export interface CounterProps {
-    className?:string
-}
-
-export const Counter:FC<CounterProps> = ({ className }) => {
-    const dispatch = useDispatch();
-    const value = useSelector<StateSchema>(getCounterValue);
-
-    const inc = () => {
-        dispatch(counterActions.increment());
-    };
-    const dec = () => {
-        dispatch(counterActions.decrement());
-    };
-
-    return (
-        <div className={classnames(classnames(className))}>
-            <h1 data-testid="value-title">{value}</h1>
-            <button data-testid="increment-btn" onClick={inc}>inc</button>
-            <button data-testid="decrement-btn" onClick={dec}>dec</button>
-        </div>
-    );
-};
+export const hz = () => {};
