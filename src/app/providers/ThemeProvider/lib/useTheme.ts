6c4e697c05ae16f4cd9a9a255c99339b1437e607
vlad <smirnov06091992@yandex.ru>
2025-05-27 11:33:19 +0500

    44 - added new theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -11,7 +11,21 @@ interface UseThemeResult {
 export const useTheme = ():UseThemeResult => {
     const { theme, setTheme } = useContext(ThemeContext);
     const toggleTheme = () => {
-        const mode = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+        let mode = Theme.LIGHT;
+
+        switch (true) {
+        case theme === Theme.DARK:
+            mode = Theme.ORANGE;
+            break;
+        case theme === Theme.ORANGE:
+            mode = Theme.LIGHT;
+            break;
+        case theme === Theme.LIGHT:
+            mode = Theme.DARK;
+            break;
+        default:
+            mode = Theme.LIGHT;
+        }
         setTheme?.(mode);
 
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, mode);
