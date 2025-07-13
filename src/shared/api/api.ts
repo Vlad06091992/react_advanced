84251f14feb1795ecde57224df31f1024c7eee26
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-07-13 17:43:11 +0500

    62 - fix axios config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,8 +2,15 @@ import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const api = axios.create({
-    baseURL: __API_URL__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY),
-    },
+    baseURL: __API_URL__
 });
+
+api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY);
+    }
+
+    return config;
+}, (error) =>
+    // Do something with request error
+    Promise.reject(error));
