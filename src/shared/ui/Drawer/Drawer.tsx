371d140289a8dba475db4bdd8b49108397d0a711
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-09-28 12:18:51 +0500

    82 добавил функционал drag and drop для шторки Drawer(шаблон для эксперимента с динамическим импортом библиотек)

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,8 +1,11 @@
-// import { classnames, Mods } from 'shared/lib/classNames/classNames';
-import React, { memo, ReactNode } from 'react';
+import React, {
+    memo, ReactNode, useCallback, useEffect
+} from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { classnames, Mods } from 'shared/lib/classnames';
-import { useModal } from 'shared/lib/hooks/useModal/useModal';
+import { classnames } from 'shared/lib/classnames';
+import { useAnimationLibs } from 'shared/lib/components/AnimationProvider';
+import { useDrag } from '@use-gesture/react';
+import { a, useSpring, config } from '@react-spring/web';
 import { Overlay } from '../Overlay/Overlay';
 import cls from './Drawer.module.scss';
 import { Portal } from '../Portal/Portal';
@@ -15,34 +18,83 @@ interface DrawerProps {
     onClose?: () => void;
 }
 
-export const Drawer = memo((props: DrawerProps) => {
+const height = window.innerHeight - 100;
+
+export const DrawerContent = memo((props: DrawerProps) => {
     const {
         className,
         children,
         onClose,
         isOpen,
-        lazy
     } = props;
-    const { theme } = useTheme();
-    const { isClosing, isMounted, closeHandler } = useModal({ onClose, isOpen });
 
-    const mods: Mods = {
-        [cls.opened]: isOpen,
-        [cls.isClosing]: isClosing,
+    const { theme } = useTheme();
+    const [{ y }, api] = useSpring(() => ({ y: height }));
+
+    const openDrawer = useCallback(() => {
+        api.start({ y: 0, immediate: false });
+    }, [api]);
+
+    useEffect(() => {
+        if (isOpen) {
+            openDrawer();
+        }
+    }, [api, isOpen, openDrawer]);
+
+    const close = (velocity = 0) => {
+        api.start({
+            y: height,
+            immediate: false,
+            config: { ...config.stiff, velocity },
+            onResolve: onClose,
+        });
     };
 
-    if (lazy && !isMounted) return null;
+    const bind = useDrag(
+        ({
+            last,
+            velocity: [, vy],
+            direction: [, dy],
+            movement: [, my],
+            cancel,
+        }) => {
+            if (my < -70) cancel();
+
+            if (last) {
+                if (my > height * 0.5 || (vy > 0.5 && dy > 0)) {
+                    close();
+                } else {
+                    openDrawer();
+                }
+            } else {
+                api.start({ y: my, immediate: true });
+            }
+        },
+        {
+            from: () => [0, y.get()], filterTaps: true, bounds: { top: 0 }, rubberband: true,
+        },
+    );
+
+    if (!isOpen) {
+        return null;
+    }
+
+    const display = y.to((py) => (py < height ? 'block' : 'none'));
 
     return (
         <Portal>
-            <div className={classnames(cls.Drawer, [className, theme, 'app_drawer'], mods)}>
-                <Overlay onClick={closeHandler} />
-                <div
-                    className={cls.content}
+            <div className={classnames(cls.Drawer, [className, theme, 'app_drawer'])}>
+                <Overlay onClick={close} />
+                <a.div
+                    className={cls.sheet}
+                    style={{ display, bottom: `calc(-100vh + ${height - 100}px)`, y }}
+                    {...bind()}
                 >
                     {children}
-                </div>
+                </a.div>
             </div>
         </Portal>
     );
 });
+
+export const Drawer = memo((props:DrawerProps) => (<DrawerContent {...props} />));
