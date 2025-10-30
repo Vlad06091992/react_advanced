d6fec700a7c69bc275656af5537ce46df1020539
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-10-30 20:47:15 +0500

    97 - пример оформления TS|JS Doc

--- src/shared/lib/hooks/useModal/useModal.tsx
+++ src/shared/lib/hooks/useModal/useModal.tsx
@@ -10,6 +10,12 @@ interface UseModalProps {
     animationDelay?: number;
 }
 
+/**
+ * Переиспользуемый хук для модальных компонентов (drawer/modal)
+ * @param animationDelay
+ * @param isOpen
+ * @param onClose
+ */
 export const useModal = ({
     onClose, isOpen, animationDelay = ANUIMATION_DELAY
 }:UseModalProps) => {
