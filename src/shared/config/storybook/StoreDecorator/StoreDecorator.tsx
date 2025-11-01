74bd5251b67dedd03c316ea04c6b28e448894dae
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-11-01 10:41:35 +0500

    101 - фикс после фикса типа

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -3,21 +3,23 @@ import { ReducersMapObject } from '@reduxjs/toolkit';
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
 // TODO разобраться почему не работает исключение для линтера -> ignoreImportPatterns: ['**/StoreProvider', '**/testing'],
 // eslint-disable-next-line
-import { loginReducer } from '@/features/AuthByUsername';
+import {loginReducer} from '@/features/AuthByUsername';
 // eslint-disable-next-line
-import { profileReducer } from '@/features/EditableProfileCard';
+import {profileReducer} from '@/features/EditableProfileCard';
 import { ReducerList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 // eslint-disable-next-line
-import { articleDetailsReducer } from '@/entities/Article';
+import {articleDetailsReducer} from '@/entities/Article';
 // eslint-disable-next-line
-import { addCommentFormReducer } from '@/features/AddCommentForm';
+import {addCommentFormReducer} from '@/features/AddCommentForm';
+// eslint-disable-next-line
+import { articleDetailsPageReducer } from '@/pages/ArticleDetailsPage/testing';
 
-const defaultAsyncReducers: ReducerList = {
+const defaultAsyncReducers: Partial<ReducerList> = {
     loginData: loginReducer,
     profileData: profileReducer,
     articleDetails: articleDetailsReducer,
     addCommentForm: addCommentFormReducer,
-    articleDetailsPage: articleDetailsReducer,
+    articleDetailsPage: articleDetailsPageReducer,
 };
 
 // eslint-disable-next-line no-undef
