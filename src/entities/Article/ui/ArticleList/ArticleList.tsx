96bada89b9a5b4b9cdd86cfdb9db5fd757a73af9
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-11-12 14:20:36 +0500

    113 - заюзал cypress

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -2,22 +2,31 @@ import { useTranslation } from 'react-i18next';
 import { HTMLAttributeAnchorTarget } from 'react';
 import { Text, TextSize } from '@/shared/ui/Text';
 import { classnames } from '@/shared/lib/classnames';
-import { ArticlesViewMode } from '../../model/types/Article';
+import {Article, ArticlesViewMode} from '../../model/types/Article';
 import cls from './ArticleList.module.scss';
+import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
+import { ArticleListItemSkeleton } from '../ArticleListItem/ArticleListItemSkeleton';
 
 interface ArticleListProps {
     className?: string
-    articles: any
+    articles: Article[]
     isLoading?: boolean
     virtualized?: boolean
     viewMode?: ArticlesViewMode
     target?: HTMLAttributeAnchorTarget
 }
 
+const getSkeletons = (view: ArticlesViewMode) => new Array(view === ArticlesViewMode.SMALL ? 9 : 3)
+    .fill(0)
+    .map((item, index) => (
+        <ArticleListItemSkeleton className={cls.card} key={index} viewMode={view} />
+    ));
+
 export const ArticleList = ({
     className,
     isLoading,
     articles,
+    target,
     viewMode = ArticlesViewMode.SMALL,
 // eslint-disable-next-line consistent-return
 }: ArticleListProps) => {
@@ -30,4 +39,22 @@ export const ArticleList = ({
             </div>
         );
     }
+
+    return (
+        <div
+            className={classnames(cls.ArticleList, [className, cls[viewMode]])}
+        >
+            {articles.map((item) => (
+                <ArticleListItem
+                    article={item}
+                    viewMode={viewMode}
+                    target={target}
+                    key={item.id}
+                    className={cls.card}
+                />
+            ))}
+            {isLoading && getSkeletons(viewMode)}
+        </div>
+
+    );
 };
